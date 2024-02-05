5601651b26b5297572d5c822e3deca23e9374943
Erik Tedfelt Lennström <erik@tedfeltlennstrom.se>
2024-02-05 19:34:50 +0100

    layout

--- keyboards/lily58/keymaps/eriklennstrom/keymap.c
+++ keyboards/lily58/keymaps/eriklennstrom/keymap.c
@@ -25,19 +25,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MPLY,
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, LSFT(KC_8),  LSFT(KC_9),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        KC_LALT, MO(_RAISE), KC_LGUI, KC_SPC, KC_ENT, KC_BSPC, MO(_LOWER), KC_RGUI
-),
+                        KC_LALT, MO(_RAISE), KC_LGUI, KC_SPC, KC_ENT, KC_RGUI, MO(_LOWER), KC_BSPC),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   -  |
+ * |      |   !  |   &  |   ?  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   -  |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |   _  |   +  |   {  |   }  |   |  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -48,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______,KC_VOLD, KC_VOLU, KC_MUTE,
   KC_F1,   KC_NUHS,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_GRV, KC_EXLM, ALGR(KC_2),   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
-  _______, _______, _______, _______, _______, _______, ALGR(KC_8), ALGR(KC_9), XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+  _______, KC_EXLM, KC_CIRC,   KC_UNDS, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
+  _______, _______, ALGR(KC_2), ALGR(KC_NUBS), _______, _______, ALGR(KC_8), ALGR(KC_9), XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
                              _______, _______, _______, _______, _______,  _______, _______, _______
 ),
 /* RAISE
@@ -68,10 +67,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, KC_MPRV, KC_MNXT, _______,
+  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY,
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-  KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, ALGR(KC_NUBS), XXXXXXX,
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  ALGR(KC_7), ALGR(KC_0),  KC_PLUS, KC_MINS, KC_NUBS, LSFT(KC_NUBS), LSFT(KC_7), KC_BSLS,
+  _______, _______, _______, _______, _______, _______,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, ALGR(KC_NUBS), XXXXXXX,
+  _______, _______, _______, _______, _______, _______,   ALGR(KC_7), ALGR(KC_0),  KC_PLUS, KC_MINS, KC_NUBS, LSFT(KC_NUBS), LSFT(KC_7), KC_BSLS,
                              _______, _______, KC_ALGR,  _______, _______,  _______, _______, _______
 ),
 /* ADJUST
